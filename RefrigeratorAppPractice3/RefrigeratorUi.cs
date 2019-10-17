d527ce9f15f0269fedb1a2da9bf50f49099441ce
nanditamandal <nanditamandal.19@gmail.com>
2019-10-17 10:31:40 +0600

    problem

---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        public Refrigeretor refrigeretor;
+        string value = "";
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            refrigeretor=new Refrigeretor();
+            refrigeretor.MaxWeight = Convert.ToInt32(maxWeightTakeTextBox.Text);
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            //refrigeretor.AddItemWeight(Convert.ToInt32(itemTextBox.Text), Convert.ToInt32(weightTextBox.Text));
+            if (refrigeretor != null)
+            {
+                if(refrigeretor.Checkadd())
+                {
+                    refrigeretor.AddItemWeight(Convert.ToInt32(itemTextBox.Text), Convert.ToInt32(weightTextBox.Text));
+                }
+               
+                string cw = refrigeretor.CurrentWeight().ToString();
+                string rw = refrigeretor.RemainWeight().ToString();
+              
+                string values = refrigeretor.item.Sum().ToString();
+                string cwp = refrigeretor.weightPerUnit.Sum().ToString();
+                string max =Convert.ToString( refrigeretor.MaxWeight);
+
+
+                if (refrigeretor.Check() == true)
+                {
+                   // refrigeretor.AddItemWeight(Convert.ToInt32(itemTextBox.Text), Convert.ToInt32(weightTextBox.Text));
+                    currentWeightTextBox.Text = cw;
+                    remainingWeightTextBox.Text = rw;
+                    showrichTextBox.AppendText(refrigeretor.Itemreturn() + "\n");
+
+
+                }
+                else
+                {
+                    
+
+                    MessageBox.Show("maximum weight is overloaded...");
+                    showrichTextBox.AppendText(values+ " "+cwp+" "+max);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("enter maximum weight!!!!");
+            }
+            //if (itemTextBox.Text=="")
+            //{
+
+            //    MessageBox.Show("enter item..");
+            //    return;
+
+            //}
+            //else if (weightTextBox.Text == "")
+            //{
+
+            //    MessageBox.Show("enter weight..");
+            //    return;
+
+            //}
+            
+            
+           //  refrigeretor.AddItemWeight(Convert.ToInt32(itemTextBox.Text), Convert.ToInt32(weightTextBox.Text));
+            
+
+           
+           
+        }
+    }
+}
---
+++ RefrigeratorAppPractice3/Refrigeretor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrigeretor
+    {
+        public int MaxWeight { set; get; }
+        public List<int> item = new List<int>();
+        public List<int> weightPerUnit = new List<int>();
+        
+         public int sum = 0;
+         int currentweight = 0;
+         string it= "";
+         string weight = "";
+        string wu="";
+
+       
+        public int rw;
+
+        public void AddItemWeight(int item, int weight)
+        {
+
+            
+                this.item.Add(item);
+                weightPerUnit.Add(weight);
+            
+           
+        }
+
+        public int CurrentWeight()
+        {
+        
+            for (int i=0; i<weightPerUnit.Count; i++)
+            {
+               currentweight= item[i] * weightPerUnit[i];
+               
+
+            }
+            sum = sum + currentweight;
+            return sum;
+            
+        }
+
+        public int RemainWeight()
+        {
+            rw = MaxWeight -sum;
+            return rw;
+        }
+        public bool Checkadd()
+        {
+
+            if (MaxWeight > sum)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public bool Check()
+        {
+            
+            if(MaxWeight<sum)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public string Itemreturn()
+        {
+            for(int i=0;i<item.Count; i++)
+            {
+                wu =Convert.ToString( item[i] * weightPerUnit[i]);
+                it= Convert.ToString(item[i]);
+                weight =Convert.ToString( weightPerUnit[i]);
+                
+            }
+            return it+","+weight+","+wu ;
+        }
+        
+
+
+    }
+}
